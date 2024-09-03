7feaf9c7ef7b912e192fee6f9651c8f7569367e4
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-09-03 23:18:16 +0900

    🔧 astro.config.mjsへのmdxインテグレーションの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro'
+import UnoCSS from 'unocss/astro';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   integrations: [
     UnoCSS({
       injectReset: true,
     }),
+    mdx(),
   ],
 });
