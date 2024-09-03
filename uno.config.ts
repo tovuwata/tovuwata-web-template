5c52f20754fd80a0a7cbb9c24dcfc32ab076a267
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-09-03 18:48:30 +0900

    🔧 uno.config.tsにUnoCSSのpresetWebFontsを追加する

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,15 @@
-import { defineConfig, presetUno } from 'unocss';
+import { defineConfig, presetUno, presetWebFonts } from 'unocss';
 
 export default defineConfig({
-  presets: [presetUno()],
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: 'bunny',
+      fonts: {
+        sans: ['Roboto', 'Noto Sans JP', 'Noto Emoji'],
+        mono: ['Roboto Mono'],
+        alpha: ['ABeeZee'],
+      },
+    }),
+  ],
 });
