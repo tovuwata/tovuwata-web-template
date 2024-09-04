792efe75f228ef32cbc7b9e5e9809ae8510d3b71
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-09-04 22:16:29 +0900

    🔧 uno.config.tsにボタンとカードの新しいショートカットを追加

--- uno.config.ts
+++ uno.config.ts
@@ -19,5 +19,11 @@ export default defineConfig({
     }),
     presetIcons(),
   ],
+  shortcuts: {
+    button:
+      'flex-inline items-center justify-center h-12 px-6 py-4 mt-4 text-2xl transition rounded-full bg-gradient-to-rb from-orange-600 to-purple-600 hover:scale-130',
+    card: 'border-t-8 rounded bg-gray-950 border-green transition hover:scale-110',
+    cardParent: 'grid gap-8 cols-1 lg:cols-3',
+  },
   transformers: [transformerDirectives()],
 });
